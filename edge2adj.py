e731e50276227c488ac00c4f74c531ce31dd46ad
Arash <arashm81@gmail.com>
2021-08-09 16:14:52 -0700

    edj2adj() conversts a list of edges to an Adjacency list (dictionay)

---
+++ edge2adj.py
@@ -0,0 +1,40 @@
+# This function converts a list of (graph) edges to an adjacency list.
+# Adjacency lists are typically used as input to DFS, BFS, etc. functions
+def edge2adj(edges):
+    adj = {}
+    for item in edges:
+        if item[0] not in adj:
+            adj[item[0]] = []
+        if item[1] not in adj[item[0]]:
+            adj[item[0]].append(item[1])
+        if item[1] not in adj:
+            adj[item[1]] = []
+        if item[0] not in adj[item[1]]:
+            adj[item[1]].append(item[0])        
+    return adj
+
+
+if __name__ == "__main__":
+    edgeList = [['A','B'],
+                ['A','M'],
+                ['A','M'],
+                ['B','F'],
+                ['F','M'],
+                ['B','C'],
+                ['C','L'],
+                ['C','K'],
+                ['C','E'],
+                ['E','N'],
+                ['N','H'],
+                ['E','H'],
+                ['B','G'],
+                ['F','G'],
+                ['G','I'],
+                ['D','I'],
+                ['G','J'],
+                ['G','H'],
+                ['G','M'],
+                ['M','D']]
+
+    print(edge2adj(edgeList))
+
