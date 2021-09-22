f38cd8e46fc09308d217a1ed9f47602ea0243cfc
Arash <arashm81@gmail.com>
2021-09-21 17:21:43 -0700

    pre/in/post/level order & left/right/zigzag views

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -29,17 +29,92 @@ class BST:
         else:
             print('Repeated value')
 
-    def show(self):
+    def show(self,view='inOrder'):
         if self.root != None:
-            self._show(self.root)
+            if view == 'preOrder':
+                self._show_preOrder(self.root)  # root left right  
+            elif view == 'inOrder':
+                self._show_inOrder(self.root)   # left root right  
+            elif view == 'postOrder':
+                self._show_postOrder(self.root) # left right root
+            elif view == 'levelOrder':
+                from collections import defaultdict
+                self.order = defaultdict(list)
+                level = 0 # root level
+                self._show_levelOrder(self.root,level)
+                for level_data in self.order.values():
+                    for val in level_data:
+                        print(val, end =' ')
+            elif view == 'zigzag':
+                output= []
+                level = 0 # root level
+                self._show_zigzag(self.root,level,output)
+                for zigzag_item in output:
+                    for val in zigzag_item:
+                        print(val, end = ' ')
+            elif view == 'left':
+                self.left_side = {}
+                height = 0 # root height
+                self._show_leftview(self.root,height)
+                for val in self.left_side.values():
+                    print(val, end =' ')
+            elif view == 'right':
+                self.right_side = {}
+                height = 0 # root height
+                self._show_rightview(self.root,height)
+                for val in self.right_side.values():
+                    print(val, end =' ')
         print()
 
-    def _show(self,cur):
+    def _show_preOrder(self,cur):
         if cur != None:
-            self._show(cur.left)
             print(str(cur.val), end =" ")
-            self._show(cur.right)
+            self._show_preOrder(cur.left)
+            self._show_preOrder(cur.right)
 
+    def _show_inOrder(self,cur):
+        if cur != None:
+            self._show_inOrder(cur.left)
+            print(str(cur.val), end =" ")
+            self._show_inOrder(cur.right)
+
+    def _show_postOrder(self,cur):
+        if cur != None:
+            self._show_postOrder(cur.left)
+            self._show_postOrder(cur.right)
+            print(str(cur.val), end =" ")
+
+    def _show_levelOrder(self,cur,level):
+        if cur != None:
+            self.order[level].append(cur.val)
+            self._show_levelOrder(cur.left,level+1)
+            self._show_levelOrder(cur.right,level+1)
+
+    def _show_zigzag(self,cur,level,output):
+        if cur != None:
+            if len(output) <= level:
+                output += [[]]
+            self._show_zigzag(cur.left,level+1,output)
+            self._show_zigzag(cur.right,level+1,output)
+            if level & 1 == 0: #level % 2 == 0
+                output[level].append(cur.val)
+            else:
+                output[level].insert(0,cur.val) #prepend
+
+    def _show_leftview(self,cur,height):
+        if cur != None:
+            if height not in self.left_side:
+                self.left_side[height] = cur.val
+                self._show_leftview(cur.left,height+1) # <-- important to call cur.left first
+                self._show_leftview(cur.right,height+1)
+
+    def _show_rightview(self,cur,height):
+        if cur != None:
+            if height not in self.right_side:
+                self.right_side[height] = cur.val
+                self._show_rightview(cur.right,height+1) # <-- important to call cur.right first
+                self._show_rightview(cur.left,height+1)                     
+            
     def height(self):
         if self.root != None:
             return self._height(self.root,0)
@@ -154,7 +229,13 @@ if __name__ == "__main__":
     bst.insert(10)       
     bst.insert(16)       
     bst.insert(50)              
-    bst.show()            # 7 8 10 16 20 50
+    bst.show()                   # 7 8 10 16 20 50  (default: view = inOrder)
+    bst.show(view='preOrder')    # 8 7 20 10 16 50
+    bst.show(view='postOrder')   # 7 16 10 50 20 8
+    bst.show(view='levelOrder')  # 8 7 20 10 50 16
+    bst.show(view='zigzag')      # 8 20 7 10 50 16
+    bst.show(view='left')        # 8 7
+    bst.show(view='right')       # 8 20 50
     print(bst.height())   # 4
     print(bst.exists(16)) # True
     bst.remove(8)         
@@ -168,4 +249,11 @@ if __name__ == "__main__":
     bst_rand.fill_randint()
     bst_rand.show()       # 0 6 12 16 19 32 37 52 53 58 63 73 83 95 99
 
+    bst_rand = BST()
+    bst_rand.fill_randint(12,-11,42) #(num_nodes, min, max)
+    bst_rand.show()       # -8 -4 -2 2 3 9 18 22 29 30 33 39
+    
     print(bst.diameter()) # 2
+
+
+    
